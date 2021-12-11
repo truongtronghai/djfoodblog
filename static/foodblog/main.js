5faf2b457bd8214e0076ae7bb6b36bc90908bd5a
Trong Hai <truongtronghai@gmail.com>
2021-12-11 12:24:54 +0700

    collect static file when debug false

---
+++ static/foodblog/main.js
@@ -0,0 +1,17 @@
+  // Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+// close the modal when clicking outside of the modal box
+// Get the modal
+var modal = document.getElementById('contactModal');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
