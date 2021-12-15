21d4e82b76d2383f2c1c7a0d5a1dd586bca815b3
Trong Hai <truongtronghai@gmail.com>
2021-12-15 20:48:26 +0700

    debug True

--- djfoodblog/settings.py
+++ djfoodblog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-+gkds=r6!ysjm83puqmp(-0k*s8z2j4g254&^o3s@v1j24mky$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['beautystuffs.herokuapp.com']
 
